b7e3dc3bb441b3d6647fbbf7af1b52e43bb299b3
mryhryki <mryhryki@gmail.com>
2022-05-10 11:08:39 +0900

    Update comment

--- ecspresso/rollback.sh
+++ ecspresso/rollback.sh
@@ -4,4 +4,4 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE}")"; pwd)"
 ecspresso rollback \
     --config="${THIS_DIR}/ecspresso.yml" \
     --envfile="${THIS_DIR}/.env" \
-    --deregister-task-definition # 今後、間違ってロールバックしたタスク定義を使われないように現行のリビジョンを削除する
+    --deregister-task-definition # 今後、間違ってロールバックしたタスク定義を使われないように現行のリビジョンをINACTIVEにする
